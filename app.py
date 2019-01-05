9d3a7cd2debb355fe5b6e0191670fc90a393841d
DUMAS <louis.dumas.m1@polytechnique.edu>
2019-01-05 19:28:13 +0100

    form page done

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from src.LN_graph import LN_graph
 import requests
-from flask import Flask, json
+from flask import Flask, json,render_template, request
 
 #%%
 url = 'https://graph.lndexplorer.com/api/graph'
@@ -15,6 +15,20 @@ graph.add_nodes(LN_nodes)
 graph.add_edges(LN_edges)
 #%%
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/result', methods=['POST', 'GET'])
+def result():
+    path, cost = graph.best_path(request.form['node1_pub'],
+                                 request.form['node2_pub'],
+                                 float(request.form['transfered_amount']),
+                                 request.form['criteria'])
+    print path
+    return render_template('result.html')
+
 @app.route('/bestpath/<criteria>/<source_node_pub>/<target_node_pub>/<transfered_amount>')
 def bestpath(criteria=None, source_node_pub=None, target_node_pub=None, transfered_amount=None):
     return json.dumps(graph.best_path(source_node_pub,
